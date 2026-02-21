3d2eae7e6a827e84e27e5c5f7d28f6a2cdb19f58
Z474AL3MN2VC <74605602+Z474AL3MN2VC@users.noreply.github.com>
2026-02-20 16:49:03 -0800

    Update build.sh

--- build_files/build.sh
+++ build_files/build.sh
@@ -9,7 +9,7 @@ set -ouex pipefail
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/43/x86_64/repoview/index.html&protocol=https&redirect=1
 
 # Install Fedora packages:
-dnf5 install -y bat blackbox-terminal btrfs-assistant fd-find lsd lutris npm ripgrep steam trash-cli zoxide
+dnf5 install -y bat blackbox-terminal btrfs-assistant fd-find lsd lutris nfs-utils npm ripgrep steam trash-cli zoxide
 # Uninstall Fedora packages:
 dnf5 remove -y malcontent ptyxis
 
