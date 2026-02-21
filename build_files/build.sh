ebf63798b27192979af011d565fa5b4fa0779c4c
Z474AL3MN2VC <74605602+Z474AL3MN2VC@users.noreply.github.com>
2026-02-21 13:21:00 -0800

    Update build.sh to remove flatpak installations
    Removed several flatpak package installations and commented them out.

--- build_files/build.sh
+++ build_files/build.sh
@@ -15,7 +15,7 @@ dnf5 install -y bat blackbox-terminal btrfs-assistant fd-find gamescope lsd lutr
 # npm i -g @continuedev/cli
 
 # Uninstall Fedora packages:
-dnf5 remove -y firefox ptyxis
+dnf5 remove -y firefox htop nvtop ptyxis
 
 # Enable COPR repos and install COPR packages:
 dnf copr enable -y atim/bottom
@@ -40,30 +40,30 @@ EOF
 dnf5 install -y codium
 
 # Install flatpak packages:
-flatpak install -y be.alexandervanhee.gradia
-flatpak install -y ca.desrt.dconf-editor
-flatpak install -y com.bitwarden.desktop
-flatpak install -y com.discordapp.Discord
-flatpak install -y com.github.Matoking.protontricks
-flatpak install -y com.github.tchx84.Flatseal
-flatpak install -y com.mastermindzh.tidal-hifi
-flatpak install -y com.mattjakeman.ExtensionManager
-flatpak install -y com.nextcloud.desktopclient.nextcloud
-flatpak install -y com.protonvpn.www
-flatpak install -y com.rustdesk.RustDesk
-flatpak install -y com.vysp3r.ProtonPlus
-flatpak install -y io.github.flattool.Ignition
-flatpak install -y io.github.flattool.Warehouse
-flatpak install -y io.github.pwr_solaar.solaar
-flatpak install -y io.github.realmazharhussain.GdmSettings
-flatpak install -y io.gitlab.librewolf-community
-flatpak install -y me.proton.Mail
-flatpak install -y net.cozic.joplin_desktop
-flatpak install -y org.deluge_torrent.deluge
-flatpak install -y org.jellyfin.JellyfinDesktop
-flatpak install -y org.libreoffice.LibreOffice
-flatpak install -y org.openrgb.OpenRGB
-flatpak install -y page.tesk.Refine
+#flatpak install -y be.alexandervanhee.gradia
+#flatpak install -y ca.desrt.dconf-editor
+#flatpak install -y com.bitwarden.desktop
+#flatpak install -y com.discordapp.Discord
+#flatpak install -y com.github.Matoking.protontricks
+#flatpak install -y com.github.tchx84.Flatseal
+#flatpak install -y com.mastermindzh.tidal-hifi
+#flatpak install -y com.mattjakeman.ExtensionManager
+#flatpak install -y com.nextcloud.desktopclient.nextcloud
+#flatpak install -y com.protonvpn.www
+#flatpak install -y com.rustdesk.RustDesk
+#flatpak install -y com.vysp3r.ProtonPlus
+#flatpak install -y io.github.flattool.Ignition
+#flatpak install -y io.github.flattool.Warehouse
+#flatpak install -y io.github.pwr_solaar.solaar
+#flatpak install -y io.github.realmazharhussain.GdmSettings
+#flatpak install -y io.gitlab.librewolf-community
+#flatpak install -y me.proton.Mail
+#flatpak install -y net.cozic.joplin_desktop
+#flatpak install -y org.deluge_torrent.deluge
+#flatpak install -y org.jellyfin.JellyfinDesktop
+#flatpak install -y org.libreoffice.LibreOffice
+#flatpak install -y org.openrgb.OpenRGB
+#flatpak install -y page.tesk.Refine
 
 # Disable COPRs so they don't end up enabled on the final image:
 # dnf5 copr disable -y ublue-os/staging
