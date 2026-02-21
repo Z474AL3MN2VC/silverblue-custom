064f3e98678934d9873eed3e92695c940782930b
Z474AL3MN2VC <74605602+Z474AL3MN2VC@users.noreply.github.com>
2026-02-20 16:44:34 -0800

    Update build script to include npm and Docker
    Added installation of npm, Docker, and VSCodium packages.

--- build_files/build.sh
+++ build_files/build.sh
@@ -9,18 +9,20 @@ set -ouex pipefail
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/43/x86_64/repoview/index.html&protocol=https&redirect=1
 
 # Install Fedora packages:
-dnf5 install -y bat blackbox-terminal btrfs-assistant fd-find lsd lutris ripgrep steam trash-cli zoxide
+dnf5 install -y bat blackbox-terminal btrfs-assistant fd-find lsd lutris npm ripgrep steam trash-cli zoxide
 # Uninstall Fedora packages:
 dnf5 remove -y malcontent ptyxis
 
 # Enable COPR repos and install COPR packages:
-# dnf5 copr enable -y ublue-os/staging
-# dnf5 install -y package
 dnf copr enable -y atim/bottom
 dnf copr enable -y lihaohong/yazi
 dnf5 install -y bottom yazi
 
-# VSCodium Repo
+# Add docker repo and install packages:
+dnf5 config-manager -y addrepo --from-repofile https://download.docker.com/linux/fedora/docker-ce.repo
+dnf5 install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
+# Add vscodium repo and install packages:
 tee -a /etc/yum.repos.d/vscodium.repo << 'EOF'
 [gitlab.com_paulcarroty_vscodium_repo]
 name=gitlab.com_paulcarroty_vscodium_repo
@@ -31,12 +33,7 @@ repo_gpgcheck=1
 gpgkey=https://gitlab.com/paulcarroty/vscodium-deb-rpm-repo/raw/master/pub.gpg
 metadata_expire=1h
 EOF
-
-dnf5 config-manager -y addrepo --from-repofile https://download.docker.com/linux/fedora/docker-ce.repo
-
-# Install third party packages.
 dnf5 install -y codium
-dnf5 install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 
 # Disable COPRs so they don't end up enabled on the final image:
 # dnf5 copr disable -y ublue-os/staging
